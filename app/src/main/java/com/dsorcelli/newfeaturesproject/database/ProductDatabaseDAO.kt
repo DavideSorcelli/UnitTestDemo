3e41d4c298c269601ee24d389ef34ba77b17c7bf
GPlmb <giulio.palomba@gmail.com>
2021-03-08 17:39:29 +0100

    Added Room Products database, not operative yet

---
+++ app/src/main/java/com/dsorcelli/newfeaturesproject/database/ProductDatabaseDAO.kt
@@ -0,0 +1,28 @@
+package com.dsorcelli.newfeaturesproject.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.dsorcelli.newfeaturesproject.models.Product
+
+@Dao
+interface ProductDatabaseDAO {
+//DAO: Data Access Object -> interf accia per operare sul DB che rappresenta
+
+    @Insert
+    fun insert(product: Product)
+
+    @Update
+    fun update(product: Product)
+
+    @Query("SELECT * FROM products WHERE id= :id")
+    fun get(id: Int): Product?
+
+    @Query("SELECT * FROM products")
+    fun getAll(id: Int): Product?
+
+    @Query("DELETE FROM products")
+    fun clear()
+
+}
