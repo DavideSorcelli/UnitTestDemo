24a42769449f1e50d70a347e506df962bb80077b
Davide <dsorcelli93@gmail.com>
2021-03-16 16:57:34 +0100

    Refactor of room db callback.

--- app/src/main/java/com/dsorcelli/newfeaturesproject/ProductApplication.kt
+++ app/src/main/java/com/dsorcelli/newfeaturesproject/ProductApplication.kt
@@ -8,6 +8,6 @@ import kotlinx.coroutines.CoroutineScope
 class ProductApplication(scope: CoroutineScope) : Application() {
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
-    val database by lazy { ShopDatabase.getDatabase(this, scope)}
+    val database by lazy { ShopDatabase.getDatabase(this)}
     val repository by lazy { ProductRepository(database.productDatabaseDAO()) }
 }
--- app/src/main/java/com/dsorcelli/newfeaturesproject/future/ShopDatabase.kt
+++ app/src/main/java/com/dsorcelli/newfeaturesproject/future/ShopDatabase.kt
@@ -6,9 +6,6 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.dsorcelli.newfeaturesproject.models.Product
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.launch
 import com.dsorcelli.newfeaturesproject.utils.vectorDrawableStringToIdMap
 
 @Database(entities = [Product::class], version = 1, exportSchema = false)
@@ -18,49 +15,13 @@ abstract class ShopDatabase : RoomDatabase() {
 
     abstract fun productDatabaseDAO(): ProductDatabaseDAO
 
-
-    private class ProductDatabaseCallback(
-        private val scope: CoroutineScope
-    ) : RoomDatabase.Callback() {
-
-        override fun onCreate(db: SupportSQLiteDatabase) {
-            super.onCreate(db)
-            INSTANCE?.let { database ->
-                scope.launch {
-                    var productDao = database.productDatabaseDAO()
-
-                    //Delete content if exists
-                    //-----
-
-                    // Add sample elements.
-                    var prod = Product(1,"London",600.00,vectorDrawableStringToIdMap.get("img_london"))
-                    productDao.insert(prod)
-                    // Add sample elements.
-                    prod = Product(2,"Rome",300.00,vectorDrawableStringToIdMap.get("img_rome"))
-                    productDao.insert(prod)
-                    // Add sample elements.
-                    prod = Product(3,"Paris",400.00,vectorDrawableStringToIdMap.get("img_paris"))
-                    productDao.insert(prod)
-                    // Add sample elements.
-                    prod = Product(4,"Barcelona",500.00, vectorDrawableStringToIdMap.get("img_barcelona"))
-                    productDao.insert(prod)
-                    // Add sample elements.
-                    prod = Product(5,"New York",900.00,vectorDrawableStringToIdMap.get("img_new_york"))
-                    productDao.insert(prod)
-
-                }
-            }
-        }
-    }
-
-        //Singleton: created just the first time
+    //Singleton: created just the first time
     companion object {
-
 
             @Volatile
             private var INSTANCE: ShopDatabase? = null
 
-            fun getDatabase(context: Context, scope: CoroutineScope): ShopDatabase {
+            fun getDatabase(context: Context): ShopDatabase {
                 // if the INSTANCE is not null, then return it,
                 // if it is, then create the database
                 return INSTANCE ?: synchronized(this) {
@@ -69,12 +30,45 @@ abstract class ShopDatabase : RoomDatabase() {
                         ShopDatabase::class.java,
                         "shop_database"
                     )
-                        .addCallback(ProductDatabaseCallback(scope))
+                        .addCallback(productDatabaseCallback)
                         .build()
                     INSTANCE = instance
                     // return instance
                     instance
                 }
             }
+
+            private val productDatabaseCallback: Callback = object : Callback() {
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+
+                    INSTANCE?.let {
+
+                        val productDao = it.productDatabaseDAO()
+
+                        //Delete content if exists
+                        //-----
+
+                        // Add sample elements.
+                        var prod = Product(1,"London",600.00,vectorDrawableStringToIdMap.get("img_london"))
+                        productDao.insert(prod)
+                        // Add sample elements.
+                        prod = Product(2,"Rome",300.00,vectorDrawableStringToIdMap.get("img_rome"))
+                        productDao.insert(prod)
+                        // Add sample elements.
+                        prod = Product(3,"Paris",400.00,vectorDrawableStringToIdMap.get("img_paris"))
+                        productDao.insert(prod)
+                        // Add sample elements.
+                        prod = Product(4,"Barcelona",500.00, vectorDrawableStringToIdMap.get("img_barcelona"))
+                        productDao.insert(prod)
+                        // Add sample elements.
+                        prod = Product(5,"New York",900.00,vectorDrawableStringToIdMap.get("img_new_york"))
+                        productDao.insert(prod)
+
+                    }
+
+                }
+            }
+
         }
     }
